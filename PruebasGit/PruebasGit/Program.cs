4087b21a06e201e6a26185a10f3c43a83aaf4aec
Brandonqr <brandonqr1@gmail.com>
2017-02-17 11:16:55 +0100

    primer comentario

---
+++ PruebasGit/PruebasGit/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebasGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esto es una prueba");
+        }
+    }
+}
